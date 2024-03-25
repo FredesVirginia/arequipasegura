b47d6691ba4c6b5241ef115a789e9191af2d1285
Virginia <WIlliam-Champion@outlook.com>
2024-03-25 04:14:38 -0300

    w

--- src/components/form/inputMaskControl/index.tsx
+++ src/components/form/inputMaskControl/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Controller } from "react-hook-form";
 import { InputProps } from "../Input";
-import _InputMask from "react-input-mask";
+
 import { Input as _Input } from "@chakra-ui/react";
 import { FormControl, FormControlProps } from "../formControl";
 import { InputMask, InputMaskProps } from "../inputMask";
